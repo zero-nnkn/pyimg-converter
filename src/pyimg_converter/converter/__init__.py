8d33b850c6892113fc3a48aca79ad029ce055517
ngxquang <nq23092016@gmail.com>
2023-07-12 15:06:33 +0700

    feature: add a heic2img converter

--- src/pyimg_converter/converter/__init__.py
+++ src/pyimg_converter/converter/__init__.py
@@ -1,12 +1,15 @@
 from .pdf2images import Pdf2ImgsConverter
+from .heic2image import Heic2ImgConverter
 
 SUPPORTED_CONVERTER = {
     'pdf': Pdf2ImgsConverter,
+    'heic': Heic2ImgConverter,
 }
 
 def get_converter(file_type: str):
     converter = {
         'pdf': Pdf2ImgsConverter,
+        'heic': Heic2ImgConverter,
     }.get(file_type)
 
     if converter is None:
