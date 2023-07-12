b2522f77d2828ac178875a12df451111e37fad22
Chi-Bien Chu <chibien408@gmail.com>
2023-07-12 15:21:05 +0700

    ✨ Add converter for PNG, JPG, TIFF

--- src/pyimg_converter/converter/__init__.py
+++ src/pyimg_converter/converter/__init__.py
@@ -1,15 +1,18 @@
 from .pdf2images import Pdf2ImgsConverter
+from .pil_converter import PILConverter
 
 SUPPORTED_CONVERTER = {
-    'pdf': Pdf2ImgsConverter,
+    "pdf": Pdf2ImgsConverter,
+    "tiff": PILConverter,
+    "png": PILConverter,
+    "jpg": PILConverter,
 }
 
+
 def get_converter(file_type: str):
-    converter = {
-        'pdf': Pdf2ImgsConverter,
-    }.get(file_type)
+    converter = SUPPORTED_CONVERTER.get(file_type)
 
     if converter is None:
-        raise ValueError(f'Unsupported file type: {file_type}. Check {SUPPORTED_CONVERTER.keys()}')
+        raise ValueError(f"Unsupported file type: {file_type}. Check {SUPPORTED_CONVERTER.keys()}")
 
     return converter()
